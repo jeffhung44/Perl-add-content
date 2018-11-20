229e7a97819364993a99d7396517bfe59dfba705
jeffhung44 <45085981+jeffhung44@users.noreply.github.com>
2018-11-20 17:37:39 +0800

    Update Perl_write_content.pl

--- Perl_write_content.pl
+++ Perl_write_content.pl
@@ -1,6 +1,10 @@
 #ARGV[0] Input file DIR;
 #ARGV[1] Output file DIR;
+<<<<<<< HEAD
 #execute perl [program] [Input DIR] 
+=======
+
+>>>>>>> parent of 0b6cc9f... Update Perl_write_content.pl
 
 use warnings;
 use strict;
@@ -15,15 +19,18 @@ my $file;
 my $file_handle;
 my $file_out;
 my $file_out_handle;
-my $fail_cnt = 0;
-my $success_cnt = 0;
+my $fail_cnt;
+my $success_cnt;
 my @fail_files;
 my @success_files;
+<<<<<<< HEAD
 my $print_something = "`define RAID_SLOT   'h0
 `define RAID_PMP    'h0
 `define RAID_MODE   'h0
 `define RAID_MCNT   'h1";      #text need to add in the file
 
+=======
+>>>>>>> parent of 0b6cc9f... Update Perl_write_content.pl
 
 mkdir($out_dir) unless(-d $out_dir); #create outout files dir;
 
@@ -31,6 +38,7 @@ opendir $dir_handle, $dir or die "can not open dir $dir :$!\n";
 opendir $out_dir_handle, $out_dir or die "can not open out_dir $out_dir :$!\n";
 
 foreach $file (readdir $dir_handle){
+<<<<<<< HEAD
         # print "there has $file in the file\n";     ######### Check files in the folder;
     
     local $/= undef;
@@ -39,9 +47,15 @@ foreach $file (readdir $dir_handle){
     next if($file =~ /^\.\.$/);             #process all folder except ..
     open $file_handle,'<',$file or die " can not open $file : $!\n";
     $file_out = "$file";                #Output file name with suffix .new
+=======
+        print "there has $file in the file\n";
+    open $file_handle,'<',$file or die " can not open $file : $!\n";
+    $file_out = "$file.new";           #Output file name with suffix .new
+>>>>>>> parent of 0b6cc9f... Update Perl_write_content.pl
     open $file_out_handle, '>>', "$out_dir/$file_out" or die " can not open $file_out :$!\n";
     #print $file_out_handle "this is the first line!\n";    #add some text to first line;
     
+<<<<<<< HEAD
     my $fullstring = do{local $/;<$file_handle>};       ###### Read Full File into String
     open $file_handle,'<',$file or die "can not open $file :$!\n";
     if ($fullstring =~ m/$print_something/g){
@@ -65,8 +79,17 @@ foreach $file (readdir $dir_handle){
             }
             #print $file_out_handle $print_something;
     }
+=======
+    print $file_out_handle "this is the first line!\n";
+    while (<$file_handle>){                 #Do somethining here.
+            print $file_out_handle $_;      
+    }
+    close $file_handle;
+>>>>>>> parent of 0b6cc9f... Update Perl_write_content.pl
     close $file_out_handle;
+    
 }
+<<<<<<< HEAD
     #show log
     if ($verbose){
     print "list of succesful files: \n";
@@ -78,6 +101,8 @@ foreach $file (readdir $dir_handle){
     print "unrocess $fail_cnt files\n";
 
 
+=======
+>>>>>>> parent of 0b6cc9f... Update Perl_write_content.pl
     close $dir_handle;
     close $out_dir_handle;
 
