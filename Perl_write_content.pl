0970ed82469e3ea3991d77eb42ae6b2b9326604e
jeffhung44 <45085981+jeffhung44@users.noreply.github.com>
2018-11-29 15:53:04 +0800

    Update Perl_write_content.pl

--- Perl_write_content.pl
+++ Perl_write_content.pl
@@ -1,14 +1,18 @@
 #ARGV[0] Input file DIR;
 #ARGV[1] Output file DIR;
-#execute perl [program] [Input DIR] 
+#execute perl program Input DIR "Content line 1" "Line 2" "and so far"
+#perl perl_write_content.pl ./ "white to first line" "white second line"
 
 use warnings;
 use strict;
 
-
+my $backup = 1;         #The process files in the new folder new
 my $verbose = 1;        #Log on off
+my $chk_content=0;
+my $log_file="perl.log";
+my $log_fh;
 my $dir = $ARGV[0];     #Input files DIR
-my $out_dir = ".\/rst"; #Output files DIR
+my $out_dir = ".\/new"; #Output files DIR
 my $dir_handle;
 my $out_dir_handle;
 my $file;
@@ -19,33 +23,49 @@ my $fail_cnt = 0;
 my $success_cnt = 0;
 my @fail_files;
 my @success_files;
-my $print_something = "`define RAID_SLOT   'h0
-`define RAID_PMP    'h0
-`define RAID_MODE   'h0
-`define RAID_MCNT   'h1";      #text need to add in the file
+my @print_something = @ARGV;      #text need to add in the file
+my @print_something_tmp;
 
+
+for(my $i=1;$i<@print_something;$i=$i+1){
+        print $ARGV[$i]."\n";
+        $print_something_tmp[$i-1] =$ARGV[$i];
+}
+my $fullstring2 =join("\n",@print_something_tmp);
+
+#foreach (@print_something){
+#    print $_."\n";
+#}
 
 mkdir($out_dir) unless(-d $out_dir); #create outout files dir;
 
 opendir $dir_handle, $dir or die "can not open dir $dir :$!\n";
-opendir $out_dir_handle, $out_dir or die "can not open out_dir $out_dir :$!\n";
+opendir $out_dir_handle, $out_dir or die "can not open out_dir $out_dir:$!\n";
 
 foreach $file (readdir $dir_handle){
         # print "there has $file in the file\n";     ######### Check files in the folder;
     
-    local $/= undef;
+        #local $/= undef;
     
     next if($file =~ /^\./);               #process all folder except . 
     next if($file =~ /^\.\.$/);             #process all folder except ..
-    open $file_handle,'<',$file or die " can not open $file : $!\n";
+    next if($file =~/new/);
+    open $file_handle,'<',"$dir/$file" or die " can not open $file : $!\n";
     $file_out = "$file";                #Output file name with suffix .new
-    open $file_out_handle, '>>', "$out_dir/$file_out" or die " can not open $file_out :$!\n";
+    open $file_out_handle, '>', "$out_dir/$file_out" or die " can not open $file_out :$!\n";
     #print $file_out_handle "this is the first line!\n";    #add some text to first line;
     
     my $fullstring = do{local $/;<$file_handle>};       ###### Read Full File into String
-    open $file_handle,'<',$file or die "can not open $file :$!\n";
-    if ($fullstring =~ m/$print_something/g){
-            print "Found same content in: ".$file."\n";
+    if($chk_content){
+        print"=====================================\n";
+        print $fullstring2."\n";
+        print"=====================================\n";
+    }
+    open $file_handle,'<',"$dir/$file" or die "can not open $file :$!\n";
+    
+
+    if ($fullstring =~ m/$fullstring2/){
+            print "Find same content in: ".$file."\n";
             $fail_cnt++;
             push(@fail_files,$file);
             while(<$file_handle>){
@@ -54,11 +74,15 @@ foreach $file (readdir $dir_handle){
             }
     }
     else{
-            print "there has no same content in: ".$file."\n";
+            print "No same content in: ".$file."\n";
             $success_cnt++;
             push(@success_files,$file);
             
-            print $file_out_handle $print_something;
+            for(my $j=1;$j<@print_something;$j=$j+1){
+                print $file_out_handle $ARGV[$j]."\n";
+            }
+
+            #print $file_out_handle @print_something."\n";
             while(<$file_handle>){
                 #print $file_out_handle "it prints something\n";
                 print $file_out_handle $_;
@@ -67,20 +91,37 @@ foreach $file (readdir $dir_handle){
     }
     close $file_out_handle;
 }
+
+
+    open $log_fh, '>', "$log_file" or die " can not open $log_file :$!\n";
+
     #show log
     if ($verbose){
-    print "list of succesful files: \n";
-    print "@success_files","\n";
+    print "List of process files: \n";
+    print $log_fh "List of process files: \n";
+    print"=====================================\n";
+    print $log_fh "Totaly ".$success_cnt." files\n";
+    print "@success_files"."\n";
+    print $log_fh "@success_files"."\n";
+    print"=====================================\n";
     print "list of unprocess files: \n";
-    print "@fail_files","\n";
+    print"=====================================\n";
+    print $log_fh "List of unprocess files: \n";
+    print $log_fh "Totaly ".$fail_cnt." files\n";
+    print "@fail_files"."\n";
+    print $log_fh "@fail_files"."\n";
+    print"=====================================\n";
     }
     print "process $success_cnt files\n";
-    print "unrocess $fail_cnt files\n";
+    print "unrocess ".$fail_cnt."files\n";
 
-
+    close $log_fh;
     close $dir_handle;
     close $out_dir_handle;
-
-    system("rm *.v");
-    system("cp ./rst/*.v ./");
-    system("rm -rf ./rst");
+    
+    
+    if (!$backup){
+    system("rm $dir/*.v");
+    system("cp $dir/new/*.v $dir");
+    system("rm -rf $dir/new");
+    }
